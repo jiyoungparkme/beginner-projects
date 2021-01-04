bfbcd8b45ce237faa61534dc045b1ccbcf4fdf2c
Jiyoung Park <63434017+jiyoungparkme@users.noreply.github.com>
2021-01-04 17:46:46 +0900

    tictactoe_2
    add game result function as win()
    
    have to add wrong input process

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,8 @@
 
-class Tictactoe():
-    
+class Tictactoe():   
     
     def __init__(self):
-        self.position_values = {"t1" : " ",
+        self.positions = {"t1" : " ",
                         "t2" : " ",
                         "t3" : " ",
                         "m1" : " ",
@@ -11,60 +10,99 @@ class Tictactoe():
                         "m3" : " ",
                         "b1" : " ",
                         "b2" : " ",
-                        "b3" : " "}       
-           
+                        "b3" : " "} 
+        
+        # for find winner                      
+        self.positions_value = {"t1" : 0,
+                        "t2" : 0,
+                        "t3" : 0,
+                        "m1" : 0,
+                        "m2" : 0,
+                        "m3" : 0,
+                        "b1" : 0,
+                        "b2" : 0,
+                        "b3" : 0}  
+    
     def basic(self):
         """
-        basic grib of tictactoe game
+        basic grid of tictactoe game
         """ 
-        self.status = self.position_values.values()   
+        self.status = self.positions.values()   
         print("\t %s | %s | %s \n\t---+---+---\n\t %s | %s | %s \n\t---+---+---\n\t %s | %s | %s "  
     % tuple(self.status))
     
     def ready(self):
         print("**** Welcome to TICTACTOE Game! ****")
         self.status = ("T1", "T2", "T3", "M1", "M2", "M3", "B1", "B2", "B3")
-        print("\t %s | %s | %s \n\t---+---+---\n\t %s | %s | %s \n\t---+---+---\n\t %s | %s | %s "  
+        print("\t%s |%s |%s \n\t---+---+---\n\t%s |%s |%s \n\t---+---+---\n\t%s |%s |%s "  
     % self.status)
-        print("\nLet's start the game!")
-        print("Player1 : O     Player2 : X")
-        print("write a position!\n")
+        
+        
 
-    def player1(self):     
+    def player1(self):
         player1 = input("player1>>>>> ").lower()
-        self.position_values[str(player1)]="O"
+        self.positions[str(player1)]="O"
         self.basic()
+        self.positions_value[str(player1)] = 1
+        
         
     def player2(self):
         player2 = input("player2>>>>> ").lower()
-        self.position_values[str(player2)]="X"
+        self.positions[str(player2)]="X"
         self.basic()
-    
+        self.positions_value[str(player2)] = 4
+     
+
     def play(self):
+        print("\nLet's start the game!")
+        print("Player1 : O     Player2 : X")
+        print("write a position!\n")
         for i in range(1,10):
-            if i % 2 == 0:
-                self.player2()
+            win = self.win()
+            if 3 in win:
+                print("Player1 WIN!")
+                break
+            elif 12 in win:
+                print("Player2 WIN!")
+                break
+            elif i == 9:
+                if i % 2 == 0:
+                    self.player2()
+                else:
+                    self.player1()
+                print("The game ended in a tie.")
             else:
-                self.player1()
-    
+                if i % 2 == 0:
+                    self.player2()
+                else:
+                    self.player1()
+
+        
     def win(self):
-        self.p = list(self.position_values.values()) # for convenience 
+        self.p = list(self.positions_value.values()) # for convenience 
         
         # row match case
-        self.r_match = [self.p[i]==self.p[i+1]==self.p[i+2] for i in [0,3,6]]
+        self.r_match = [self.p[i]+self.p[i+1]+self.p[i+2] for i in [0,3,6]]
 
         # column match case
-        self.c_match = [self.p[i]==self.p[i+3]==self.p[i+6] for i in [0,1,2]]
+        self.c_match = [self.p[i]+self.p[i+3]+self.p[i+6] for i in [0,1,2]]
 
         # diagonal match case
-        self.d_match = [self.p[0]==self.p[4]==self.p[8], self.p[2]==self.p[4]==self.p[6]]
+        self.d_match = [self.p[0]+self.p[4]+self.p[8], self.p[2]==self.p[4]==self.p[6]]
 
         self.result = self.r_match + self.c_match+ self.d_match
-        print(self.result)
+        return self.result
+
 a = Tictactoe()
-a.ready()
-a.play()
-a.win()
+while True:
+
+    a.ready()
+    answer = input("Do you wanna play? y/n >> ").lower()
+    if answer == "y":    
+        a.play()
+    else:
+        print("Bye!")
+        break
 
 
     
