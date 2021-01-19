87cf26b7df5873f3092c5c5e7ffeac83774d2470
jiyoungparkme <63434017+jiyoungparkme@users.noreply.github.com>
2021-01-19 21:41:42 +0900

    Update create_frame.py
    gui frame complete

---
+++ gui_basic/create_frame.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.title("Image collaboration!")
+root.geometry("640x480") # width * height
+# root.geometry("640x480+100+300") # width * height x, y
+
+root.resizable(False, False) # width, height fixed
+
+root.mainloop()
